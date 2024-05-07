fac69ba7340b15959da7e96c5e5069793b4a6ebd
akhivyk <akhivyk@solvd.com>
2024-05-07 14:13:23 +0200

    init

--- src/componenets/Form/Form.jsx
+++ src/componenets/Form/Form.jsx
@@ -1,6 +1,6 @@
 import React, {useCallback, useEffect, useState} from 'react';
 import './Form.css';
-import {useTelegram} from "../../hooks/useTelegram";
+import {useTelegram} from "../hooks/useTelegram";
 
 const Form = () => {
 
